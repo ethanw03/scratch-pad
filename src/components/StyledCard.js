77b6c0599fe1011f12b1db4d9d6c8e6d8f0aae4d
ethanw03 <ethanwong98@yahoo.ca>
2023-04-29 02:35:58 -0700

    add message as a prop and styling

--- src/components/StyledCard.js
+++ src/components/StyledCard.js
@@ -1,9 +1,26 @@
-import { Card } from "@chakra-ui/react";
+import { Box, Card, Flex, Input, Spacer, Text } from "@chakra-ui/react";
 
-const StyledCard = ({ children }) => {
+import StyledCardItem from "./StyledCardItem";
+
+const StyledCard = ({}) => {
   return (
     <Card w="768px" h="514px" padding="6">
-      {children}
+      <Flex direction="column" justifyContent="center">
+        <Box>
+          <StyledCardItem message="Wow this is such a great design!" />
+          <StyledCardItem message="Chakra ui is awesome!" />
+        </Box>
+
+        <Spacer />
+        <Box position="absolute" bottom="0" marginBottom="20px">
+          <Input
+            textAlign="left"
+            width="722px"
+            height="80px"
+            placeholder="Start typing here"
+          />
+        </Box>
+      </Flex>
     </Card>
   );
 };
--- src/components/StyledCardItem.js
+++ src/components/StyledCardItem.js
@@ -1,3 +1,20 @@
-const StyledCardItem = ({ children }) => {
-  return <div className="card-item">{children}</div>;
+import { Box, Card, Flex, Text } from "@chakra-ui/react";
+
+const StyledCardItem = ({ message }) => {
+  return (
+    <Flex>
+      <Text
+        border="1px"
+        borderColor="gray.200"
+        borderRadius="8px"
+        mb="20px"
+        width="738px"
+        height="75px"
+      >
+        <Box margin="6px">{message}</Box>
+      </Text>
+    </Flex>
+  );
 };
+
+export default StyledCardItem;
