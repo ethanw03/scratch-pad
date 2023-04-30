4d40924a842722e35e8fd63d003d0d00dbcdd931
ethanw03 <ethanwong98@yahoo.ca>
2023-04-30 16:40:23 -0700

    notes now handled by state on Enter

--- src/components/StyledCard.js
+++ src/components/StyledCard.js
@@ -1,25 +1,45 @@
 import { Box, Card, Flex, Input, Spacer, Text } from "@chakra-ui/react";
 
 import StyledCardItem from "./StyledCardItem";
+import { useState } from "react";
 
 const StyledCard = ({}) => {
-  const messages = [
-    "Wow this is such a great design!",
-    "Chakra ui is awesome!",
-  ];
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState("");
+
+  const handleChange = (event) => {
+    setNote(event.target.value);
+  };
+
+  const handleAddNote = () => {
+    setNotes([note, ...notes]);
+    setNote("");
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleAddNote();
+    }
+  };
 
   return (
     <Card w="768px" h="514px" padding="6">
       <Flex direction="column" justifyContent="center">
         <Box>
-          {messages.map((message) => (
-            <StyledCardItem key="styledCardItem__id" message={message} />
+          {notes.map((note, index) => (
+            <StyledCardItem key={`styledCardItem__${index}`} message={note} />
           ))}
         </Box>
 
         <Spacer />
         <Box position="absolute" bottom="0" marginBottom="20px">
           <Input
+            type="text"
+            id="note"
+            name="note"
+            value={note}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
             textAlign="left"
             width="722px"
             height="80px"
